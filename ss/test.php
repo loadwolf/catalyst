d769619c265371d79443ba86b3caf51a3237b5fb
vortexcoaching <vortexcoaching@hotmail.com>
2019-03-11 01:24:43 +1100

    CLI -MII

--- ss/test.php
+++ ss/test.php
@@ -16,25 +16,40 @@ but the database won't be altered.
 • --help – which will output the above list of directives with details.
 
 <?php
-
+$data = [];
 for($i = 0; $i < $argc; $i++){
     //var_dump($argv);
+    
     switch ($argv[$i]) {
+        case '--file':
+            # code...
+            echo "--dry_run = TRUE";
+            array_push($data['file'],$data['file'] = $argv[$i+1]);
+            break;
         case '--create_table':
             # code...
             echo "--create_table = ".$argv[$i+1];
+            array_push($data['table'],$data['table'] = $argv[$i+1]);
             break;
         case '--dry_run':
             # code...
             echo "--dry_run = TRUE";
+            array_push($$data['dry_run'],$data['dry_run'] = true);
             break;
         case '-u':
             # code...
-            echo "--u = ".$argv[$i+1];
+            echo "-u = ".$argv[$i+1];
+            array_push($data['username'],$data['username']= $argv[$i+1]);
+            break;
+        case '-p':
+            # code...
+            echo "-u = ".$argv[$i+1];
+            array_push($data['username'],$data['username']= $argv[$i+1]);
             break;
         case '-h':
             # code...
-            echo "--h = ".$argv[$i+1];
+            echo "-h = ".$argv[$i+1];
+            array_push($data['hostname'],$data['hostname'] = $argv[$i+1]);
             break;    
         case '--help':
             # code...
@@ -57,5 +72,7 @@ for($i = 0; $i < $argc; $i++){
             # code...
             break;
     }
-    print("\n");
+    
 }
+
+print_r($data);
