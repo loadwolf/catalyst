ae3d858dd0b3bb90c1da0b20187dcb19480d4ab2
shannonpolokmq <shannon.polok@mq.edu.au>
2019-03-11 14:33:27 +1100

    Update python.bat

--- ss/python.bat
+++ ss/python.bat
@@ -1,4 +1,4 @@
-$env:Path = "C:\Users\mq20111141\AppData\Local\Programs\Python\Python37-32";
+$env:Path = "C:\Users\xxxxxxx\AppData\Local\Programs\Python\Python37-32";
 
 $env:Path = "C:\wamp64\bin\php\php7.2.14";
 
